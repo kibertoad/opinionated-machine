c808d1f38adfa333f9365ee1490d58e1adf128d8
Igor Savin <iselwin@gmail.com>
2025-03-28 01:04:48 +0200

    Fix linting

--- index.ts
+++ index.ts
@@ -4,9 +4,25 @@ export {
   type registerDependenciesParams,
 } from './lib/DIContext.js'
 export { AbstractModule } from './lib/AbstractModule.js'
-export { ENABLE_ALL, resolveJobQueuesEnabled, isJobQueueEnabled, isMessageQueueConsumerEnabled, isJobWorkersEnabled } from './lib/diConfigUtils.js'
+export {
+  ENABLE_ALL,
+  resolveJobQueuesEnabled,
+  isJobQueueEnabled,
+  isMessageQueueConsumerEnabled,
+  isJobWorkersEnabled,
+} from './lib/diConfigUtils.js'
 export { AbstractController } from './lib/AbstractController.js'
-export { asJobQueueClass, asJobWorkerClass, asMessageQueueHandlerClass, asControllerClass, asSingletonClass } from './lib/resolverFunctions.js'
+export {
+  asJobQueueClass,
+  asJobWorkerClass,
+  asMessageQueueHandlerClass,
+  asControllerClass,
+  asSingletonClass,
+} from './lib/resolverFunctions.js'
 
-export type { JobQueueModuleOptions, MessageQueueConsumerModuleOptions, JobWorkerModuleOptions } from './lib/resolverFunctions.js'
+export type {
+  JobQueueModuleOptions,
+  MessageQueueConsumerModuleOptions,
+  JobWorkerModuleOptions,
+} from './lib/resolverFunctions.js'
 export type { InferRequestFromContract } from './lib/typeUtils.js'
